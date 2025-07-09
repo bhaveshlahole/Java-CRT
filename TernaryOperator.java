9575e329cfd5ab7872e0d33b9e049bb8bc214350
BHAVESH NANDKUMAR LAHOLE <149697798+bhaveshlahole@users.noreply.github.com>
2025-07-09 08:16:40 +0530

    Add files via upload

---
+++ TernaryOperator.java
@@ -0,0 +1,13 @@
+package com.pote.engg.operators;
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+
+		int a = 10;
+		int b = 20;
+		int result = (a>b)?a:b;
+		System.out.println(result);
+	}
+
+}
---
+++ UnaryOperator.java
@@ -0,0 +1,20 @@
+package com.pote.engg.operators;
+
+public class UnaryOperator {
+
+	public static void main(String[] args) {
+		int num1 = 10;
+		System.out.println(num1 ++); // num1 = num1 + 1
+	
+		System.out.println(++num1); // num1 = 1 + num1
+		
+		System.out.println(--num1);
+		System.out.println(num1 --);  
+		
+		System.out.println(num1--);
+		System.out.println(num1);
+		
+	
+	}
+
+}
